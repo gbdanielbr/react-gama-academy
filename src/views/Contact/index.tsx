b51975ec203dba19109b0faa983fe5740a7da836
Daniel M <daniel.milli@hotmail.com>
2021-11-22 15:47:15 -0300

    Created sign in view

--- src/views/Contact/index.tsx
+++ src/views/Contact/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Link } from "react-router-dom"
-
-// import { Container } from './styles';
+import { Container } from './style';
 
 const Contact: React.FC = () => {
   return (
-    <div>
-      <h1>Contact</h1>
-      <Link to="/">Voltar para Home</Link>
-    </div>
+    <Container>
+      <div className="form-wrapper">
+        <h1>Contact</h1>
+        <div className="card">
+          <form onSubmit={() => {}}>
+            <input type="text" placeholder="nome"/>
+            <input type="text" placeholder="e-mail"/>
+            <input type="submit" placeholder="cadastrar"/>
+          </form>
+        </div>
+        <Link to="/">Voltar para Home</Link>
+      </div>
+    </Container>
   );
 }
 
