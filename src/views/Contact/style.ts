aaf7d9d01d271565511e30fd13b99de3c8164d19
Daniel M <daniel.milli@hotmail.com>
2021-11-22 15:47:41 -0300

    contact styles

---
+++ src/views/Contact/style.ts
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  background-color: lightgreen;
+  color: #fff;
+
+  .form-wrapper{
+    display: grid;
+    text-align: center;
+
+    .card{
+      background-color: #fff;
+      padding: 1.2rem;
+      border-radius: 1rem;
+
+      form{
+        display: grid;
+
+        input{
+          margin: .7rem 0;
+        }
+      }
+    }
+  }
+`
